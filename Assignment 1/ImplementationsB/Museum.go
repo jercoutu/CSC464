30caab59c3dae40a89bf68954405777179310ae6
jercoutu <43969050+jercoutu@users.noreply.github.com>
2018-10-08 12:14:41 -0700

    Update Museum.go

--- Assignment 1/ImplementationsB/Museum.go
+++ Assignment 1/ImplementationsB/Museum.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"sync"
 	"time"
 )
@@ -19,9 +19,9 @@ var (
 
 func museum() {
 	for ch, ok := <-lobby; ok ; ch, ok = <-lobby {
-		log.Println("Customer", <-ch, "enters the Museum.")
+		fmt.Println("Customer", <-ch, "enters the Museum.")
 		time.Sleep(500)
-		log.Println("Customer exists the museum")
+		fmt.Println("Customer exists the museum")
 		ch <- 0
 	}
 }
@@ -29,23 +29,23 @@ func museum() {
 func customer(id int) {
 	defer wg.Done()
 	ch := make(chan int)
-	log.Println("Customer", id, "Wants to enter the Museum.")
+	fmt.Println("Customer", id, "Wants to enter the Museum.")
 	select {
 	case lobby <- ch:		
 		ch <- id
 		<- ch
 	default:
-		log.Println("Customer", id, "decides today isn't a good day for a Museum.")
+		fmt.Println("Customer", id, "decides today isn't a good day for a Museum.")
 	}
 }
 
 func main() {
 	wg.Add(nCustomers)
 	go museum()
-	log.Println("Museum Open")
+	fmt.Println("Museum Open")
 	for i := 0; i < nCustomers; i++ {
 		go customer(i)
 	}
 	wg.Wait()
-	log.Println("Museum closed")
+	fmt.Println("Museum closed")
 }
